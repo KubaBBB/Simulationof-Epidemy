8de588e7abbb986c69080ae2724240f58db2cf9d
KubaBBB <jakubboliglowa1996@gmail.com>
2018-06-03 11:26:49 +0200

    refactored code

--- Automated Cells/Automated_Cells.py
+++ Automated Cells/Automated_Cells.py
@@ -3,7 +3,7 @@ from Grid import *
 from matplotlib import pyplot as plt
 import numpy as np
 
-def main(size):
+def main(size, colors):
     print('Hello Barszczyk Opierdalaczu')
     
     grid = Grid(size)
@@ -22,67 +22,28 @@ def main(size):
 
     plt.hold(True)
         
-    colours = ['white','green','yellow','orange','black']
-
     X = []
     Y = []
-    #print (grid.get_states_colors())
-    
-    #for index, row in enumerate( grid.get_states_colors() ):
-    #    #print (row)
-    #    npArr = np.array(row)
-    #    for i in range(grid.get_grid_size()):
-    #        x_ins = np.where(npArr[i] == 'yellow')[0]
-    #        y_ins = np.array([index for _ in range( np.count_nonzero (x_ins) ) ] )
-
-    #    X = np.insert(X, len(X), x_ins )
-    #    Y = np.insert(Y, len(Y), y_ins )
-    #    print (np.count_nonzero(x_ins))
-    #    print (np.count_nonzero(y_ins))
-    #print (X)
-    ##print (np.count_nonzero(X))
-    #print (Y)
     states_matrix = np.array(grid.get_states_colors())
-    for item in colours:
+
+    for color in colors:
         for i in range(grid.get_grid_size()):
             for j in range(grid.get_grid_size()):
-                if states_matrix[i][j] == item:
+                if states_matrix[i][j] == color:
                     X = np.insert(X, len(X),i)
                     Y = np.insert(Y, len(Y),j)
-        plt.scatter( X, Y, marker = 's', c = item)
-        print(len(X))
-    
-        print(len(Y))
-        
+
+        plt.scatter( X, Y, marker = 's', c = color)
+
         X=[]
         Y=[]
-
-
-    ##print (X)
-    
-    #print (np.count_nonzero(Y))
-    #for colour in colours:
-    #    for i in range(size):
-    #        coordinates = [coordinates, ( np.where( colour ==
-    #        grid.get_states_colors()[i])[0] )]
-    #    #print (np.where( colour == grid.get_states_colors())[0])
-    #    print (coordinates)
-
-    #values = np.array(['s','a','s','f','s','f','s','s','f','s'])
-    #searchval = 3
-    #ii = np.where(values == 'f')[0]
-    #print (ii)
-
-    #plt.plot( coordinates(:,1), coordinates(:,2), marker = 's', markersize =
-    #5, color = colour, label ='point')
-    
+   
     #plt.grid()
     plt.show()
     
 
-
-
 if __name__ == "__main__":
     size = 30
-    main(size)
+    colors = ['white','green','yellow','orange','black']
+    main(size, colors)
                     
