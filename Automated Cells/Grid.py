0e4ab6efdc5e1d487f6c1182a4ece2025e37e087
KubaBBB <jakubboliglowa1996@gmail.com>
2018-06-03 11:21:46 +0200

    Optimalized ploting cells for each states

--- Automated Cells/Grid.py
+++ Automated Cells/Grid.py
@@ -8,18 +8,23 @@ class Grid:
         """Return a Customer object Grid type, size of grid: size x size *."""
 
         self.size = size
-        self.matrix = [[Cell(random.uniform(0.0,1500.0), random.uniform(0.01, 0.10)) for _ in range(size)] for i in range(size)]
-        
+        self.cells = [[Cell(random.uniform(0.0,1500.0), random.uniform(0.01, 0.10)) for _ in range(size)] for i in range(size)]
+        self.states = [['' for _ in range(size)] for i in range(size)]
 
     def perform_next_step(self):
         for i in range(self.get_grid_size()):
             for j in range(self.get_grid_size()):
-                self.matrix[i][j].calculate_population()
-                self.matrix[i][j].calculate_mortality()
+                self.cells[i][j].calculate_population()
+                self.cells[i][j].calculate_mortality()
+                self.states[i][j] = self.cells[i][j].get_plot_color()
                 
-    def extend_area(self, size):
+    def extended_area(self, size):
+        
         pass
 
 
     def get_grid_size(self):
         return self.size
+
+    def get_states_colors(self):
+        return self.states
