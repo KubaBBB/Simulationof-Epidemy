7fb47e41a4aab7554730a439ae71bf5ab6932bd4
OptimusPrime <maciejblaszczyk1996@gmail.com>
2018-06-04 18:03:26 +0200

    figures' shapes changed

--- Automated Cells/Automated_Cells.py
+++ Automated Cells/Automated_Cells.py
@@ -1,27 +1,22 @@
-from sys import *
 from Grid import *
 from matplotlib import pyplot as plt
 import numpy as np
-from copy import deepcopy
 import time
-import string
+
 
 def main(size, colors, max_iteration):
     print('Hello Barszczyk Opierdalaczu')
     grid = Grid(size)
 
-    plt.hold(True)
-
     X = []
     Y = []
     iteration = 0
     while iteration < max_iteration :
-        
         start = time.time()
 
         grid.perform_next_step(colors)
         mapped_states = np.array(grid.get_states_colors())
-
+        plt.figure(figsize=(10, 10))
         for color in colors:
             if iteration is not 0:
                 grid.reload_grid()
@@ -30,22 +25,21 @@ def main(size, colors, max_iteration):
                     if mapped_states[i][j] == color:
                         X = np.insert(X, len(X),i)
                         Y = np.insert(Y, len(Y),j)
-            plt.scatter(X, Y, marker = 's', c = color)
-            
+            plt.scatter(X, Y, s=100, marker = 's', c = color)
             X = []
             Y = []
 
         end = time.time()
-        print('Time of performing next iteration: ' + str(end - start))        
+        print('Time of performing next iteration: ' + str(end - start))
+        plt.axis([-1, size, -1, size])
         plt.title('Iteration: ' + str(iteration))
         plt.xlabel('X') 
         plt.ylabel('Y')
         plt.show()
-        iteration+=1
+        iteration += 1
         
 if __name__ == "__main__": 
-    size = 20
+    size = 50
     colors = ['white','green','yellow','orange','black','grey', 'blue']
-    max_iteration = 10
+    max_iteration = 20
     main(size, colors, max_iteration)
-                    
